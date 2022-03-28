143e3063dc46c7d79d14d9a00ac2a5dcd983addf
zdacol <bzdancewicz482@technikumkreatywne.pl>
2022-03-28 10:09:54 +0200

    Komentarz

--- liczby pierwsze/liczby pierwsze.cpp
+++ liczby pierwsze/liczby pierwsze.cpp
@@ -3,9 +3,33 @@
 using namespace std;
 int main()
 {
-	int p;
-	cout << p; 
-	for: 2 >> p-1
+	int a;
+	int b;
+	cout << "Podaj pierwszą liczbe";
+	cin >> a;
+	cout << "Podaj drugą liczbe";
+	cin >> b;
+	while (a != b) {
+		if (a < b) {
+			b = b - a;
+		}
+		else {
+			a = a - b;
+		}
+	}
+	############
+	int c;
+	c = a * b;
+	while
+	if (a < b) {
+		b = b - a;
+	}
+	else {
+		a = a - b;
+	}
+	#####################
+	cout << "NWD to ";
+	cout << a;
 
 
 
