7aa0c7b51e5a66ca57c89ea90aa09c144d1a8ec7
zdacol <bzdancewicz482@technikumkreatywne.pl>
2022-04-04 10:08:26 +0200

    Komentarz

---
+++ szyfr cezara/szyfr cezara/szyfr cezara.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+using namespace std;
+void Funkcja1(int a, int b);
+int main()
+{
+	int klucz;
+	char tab[200];
+	cin >> tab;
+	int dl;
+	dl = strlen (tab);
+
+
+
+	if (!(klucz >= -26, klucz <= 26));
+	{return};
+	if (klucz >= 0);
+	{
+		for (int i = 0;i < dl; i++;)
+		{
+			if (tab + klucz <= z);
+			{
+				tab += klucz;
+			}
+			else { tab = tab + klucz - 26 };
+		}
+
+
+	}
+}
+
