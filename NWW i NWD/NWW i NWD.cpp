52f48ceca78522e980c8c1029e9c33f3cf5a69d2
zdancol <101553941+zdancol@users.noreply.github.com>
2022-05-04 17:18:24 +0200

    Add files via upload

---
+++ NWW i NWD/NWW i NWD.cpp
@@ -0,0 +1,48 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout << "Podaj dwie liczby do NWD ";
+	cout << endl;
+	cout << "wpisz pierwsza liczbe";
+	cout << endl;
+	cin >> a;
+	int b;
+	cout << "wpisz druga liczbe";
+	cout << endl;
+	cin >> b;
+	int iloczyn;
+	while (a != b)
+	{
+		if (a < b) {
+			b = b - a;
+		}
+		else { a = a - b; }
+	}
+	cout << "NWD tych dwuch liczb to ";
+	cout << a;
+	cout << endl;
+	cout << endl;
+	cout << "Podaj dwie liczby do NWW ";
+	cout << endl;
+	cout << "wpisz pierwsza liczbe ";
+	cin >> a;
+	cout << "wpisz druga liczbe ";
+	cin >> b;
+
+	iloczyn = a * b;
+
+	while (a != b) {
+		if (a < b) {
+			b = b - a;
+		}
+		else { a = a - b; };
+	}
+	int nww = iloczyn / a;
+	cout << "NWW tych dwoch liczb to ";
+	cout << nww;
+}
+
